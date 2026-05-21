bb3a87e105f4e273ea09ca3bf40bb6f55a312d8d
buli-net <hvlrcb@gmail.com>
2026-05-21 20:46:20 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/btcwallet/MainActivity.kt
+++ app/src/main/java/com/example/btcwallet/MainActivity.kt
@@ -1,18 +1,18 @@
 package com.example.btcwallet
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val tv = TextView(this)
-        tv.text = "APP MOI - BTCWALLET2"
-        tv.textSize = 32f
-        tv.setPadding(50,200,50,200)
-        setContentView(tv)
-        Toast.makeText(this, "DAY LA BAN MOI", Toast.LENGTH_LONG).show()
+
+        // ÉP XÓA VÍ CŨ - bỏ qua mật khẩu
+        WalletManager.deleteWallet(this)
+
+        // Vào thẳng màn tạo/khôi phục ví
+        startActivity(Intent(this, CreateImportActivity::class.java))
+        finish()
     }
 }
